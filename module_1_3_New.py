8135be67cf980d409e329cfecd28ea9b9e2a8e4b
Aleksey <poteshkinANVDK@gmail.com>
2024-11-06 14:07:50 +0300

    Add files via upload

---
+++ module_1_3_New.py
@@ -0,0 +1,10 @@
+Name = 'Aleksey'
+print('Name: ', Name)
+Age = 21
+print('Age: ', Age)
+S = input('Введите число ')
+New_Age= int(Age) + int(S)
+print('New_Age: ', New_Age)
+is_student = True
+if is_student == True:
+     print('is_student: True')
